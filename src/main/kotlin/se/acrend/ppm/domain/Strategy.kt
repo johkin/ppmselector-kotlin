19b5f5ea6f46cd7aebd5a6575d432f364ebd5982
Johan Kindgren <johan.kindgren@gmail.com>
2017-11-07 16:02:49 +0100

    lagt till månads-strategi

--- src/main/kotlin/se/acrend/ppm/domain/Strategy.kt
+++ src/main/kotlin/se/acrend/ppm/domain/Strategy.kt
@@ -9,32 +9,37 @@ enum class Strategy(val parameterName: String, val description: String, val sort
             return true
         }
     },
-    OneMonth("Month_1", "En månad", 1) {
+    OneMonth("Month_1", "En månad med veckobegräsning", 1) {
         override fun includeFund(fund: FundInfo): Boolean {
             return fund.growthWeek >= 0
         }
     },
-    ThreeMonthNoPrecidate("Month_3", "Tre månader utan begräsning", 2) {
+    OneMonthNoPredicate("Month_1", "En månad utan begränsning", 2) {
         override fun includeFund(fund: FundInfo): Boolean {
             return true
         }
     },
-    ThreeMonthOneMonthPredicate("Month_3", "Tre månader med månadsbegränsning", 3) {
+    ThreeMonthNoPrecidate("Month_3", "Tre månader utan begräsning", 3) {
+        override fun includeFund(fund: FundInfo): Boolean {
+            return true
+        }
+    },
+    ThreeMonthOneMonthPredicate("Month_3", "Tre månader med månadsbegränsning", 4) {
         override fun includeFund(fund: FundInfo): Boolean {
             return fund.growthMonth >= 0
         }
     },
-    ThreeMonth("Month_3", "Tre månader med veckobegränsning", 4) {
+    ThreeMonth("Month_3", "Tre månader med veckobegränsning", 5) {
         override fun includeFund(fund: FundInfo): Boolean {
             return fund.growthWeek >= 0 && fund.growthMonth >= 0
         }
     },
-    SixMonthNoPrecidate("Month_6", "Sex månader utan begräsning", 5) {
+    SixMonthNoPrecidate("Month_6", "Sex månader utan begräsning", 6) {
         override fun includeFund(fund: FundInfo): Boolean {
             return true
         }
     },
-    CurrentYear("YTD", "Innevarande år", 6) {
+    CurrentYear("YTD", "Innevarande år", 7) {
         override fun includeFund(fund: FundInfo): Boolean {
             return true
         }
