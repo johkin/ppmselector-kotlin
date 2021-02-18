09d7103eda9da1137db59545d14b1ec30ba47fc0
Johan Kindgren <johan.kindgren@farewell.today>
2021-02-18 22:06:35 +0100

    Insecure Truststore

--- src/main/kotlin/se/acrend/ppm/config/AppConfig.kt
+++ src/main/kotlin/se/acrend/ppm/config/AppConfig.kt
@@ -1,5 +1,6 @@
 package se.acrend.ppm.config
 
+import io.netty.channel.ChannelOption
 import io.netty.handler.ssl.SslContextBuilder
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory
 import org.springframework.context.annotation.Bean
@@ -8,6 +9,8 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 import reactor.netty.tcp.TcpClient
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @Configuration
 class AppConfig {
@@ -20,8 +23,9 @@ class AppConfig {
             .trustManager(InsecureTrustManagerFactory.INSTANCE)
             .build()
 
-        val tcpClient = TcpClient.create().secure { sslProviderBuilder -> sslProviderBuilder.sslContext(sslContext) }
-        val httpClient = HttpClient.from(tcpClient)
+        val tcpClient = TcpClient.create()
+            .secure { sslProviderBuilder -> sslProviderBuilder.sslContext(sslContext) }
+        val httpClient = HttpClient.from(tcpClient).responseTimeout(Duration.ofSeconds(30))
 
         return WebClient.builder().clientConnector(ReactorClientHttpConnector(httpClient)).build()
     }
